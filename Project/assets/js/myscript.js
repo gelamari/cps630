7ac068fa63392f8d8ab34415c028761aa3c31076
unknown <itsbasictome@gmail.com>
2020-02-26 17:53:30 -0500

    Added the js folder and my css file

---
+++ Project/assets/js/myscript.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $('select').on('change', function() {
+    var target = $(this).find(":selected").attr("data-target");
+    var id = $(this).attr("id");
+    $("div[id^='" + id + "']").hide();
+    $("#" + id + "-" + target).show();
+  });
+});
