290c11829e7939738dd546e29ed7018b2abe71ab
unknown <itsbasictome@gmail.com>
2020-02-27 15:44:48 -0500

    Modified about content

--- Project/about.php
+++ Project/about.php
@@ -9,8 +9,37 @@
     include 'assets/includes/nav.php';
     ?>
 
-    <div>
-        <p>Hi, we are team 25?</p>
-    </div>
-
+    <div class="container">
+    	<div class="col s12 m7">
+    		<h2 class="header">Team 25</h2>
+    		<div class="card horizontal">
+      			<!-- <div class="card-image">
+        			<img src="https://lorempixel.com/100/190/nature/6">
+      			</div> -->
+      			<div class="card-stacked">
+        			<div class="card-content">
+          				<h5>Nicholas DeSantos</h5>
+          				<ul>
+          					<li>• Created the Shopping Cart page</li>
+          					<li>• Implemented search bar</li>
+          					<li>• Managed project</li>
+          				</ul>
+          				<h5>Angela Gavin</h5>
+          				<ul>
+          					<li>• Created the Read More page</li>
+          					<li>• Managed the database</li>
+          					<li>• Wrote report</li>
+          				</ul>
+          				</p>
+          				<h5>Alison Der</h5>
+          				<p>Created the Home page and styled whole website with css.
+          				</p>
+        			</div>
+        			<!-- <div class="card-action">
+          				<a href="#">This is a link</a>
+        			</div> -->
+      			</div>
+    		</div>
+  		</div>
+	</div>
 <body>
