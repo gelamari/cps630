19b804ee34c68006b0c368550d7d98e85e7afb86
unknown <itsbasictome@gmail.com>
2020-02-28 02:27:39 -0500

    updated about

--- Project/about.php
+++ Project/about.php
@@ -11,68 +11,53 @@
 
     <div class="container">
     	<div class="col s12 m7">
-    		<h2 class="header">Team 25</h2>
+    		<h2 class="header">About Us</h2>
     		<div class="card horizontal">
-      			<!-- <div class="card-image">
-        			<img src="https://lorempixel.com/100/190/nature/6">
-      			</div> -->
-      			<div class="card-stacked">
-        			<div class="card-content">
-        				<ul class="collection" style="border: none;">
-    <li class="collection-item avatar">
-      <img src="assets/img/dog.png" class="circle">
-      <span class="title">Nick De Santo</span>
-      <p>
-        Hi! Welcome to our travel website! In order for this website to be responsive and pretty, we used MaterializeCSS, Ajax, Javascript, and JQuery. 
-      <ul>
-        <li>• Created the Shopping Cart Page</li>
-        <li>• Implemented search bar</li>
-        <li>• Managed project</li>
-      </ul></p>
-    </li>
-    <li class="collection-item avatar">
-      <img src="assets/img/dog1.png" class="circle">
-      <span class="title">Angela Gavin</span>
-      <p><ul>
-            <li>• Created the Read More page</li>
-            <li>• Managed the database</li>
-            <li>• Wrote report</li>
-      </ul></p>
-    </li>
-    <li class="collection-item avatar">
-      <img src="assets/img/dog2.png" class="circle">
-      <span class="title">Alison Der</span>
-      <p><ul>
-          <li>• Created the Home page</li>
-          <li>• Styled whole website with css</li>
-      </ul></p>
-    </li>
-  </ul>
-        				<!--<img src="assets/img/dog.png" style="width: 100px">
-          				<h5>Nicholas DeSantos</h5>
-          				<ul>
-          					<li>• Created the Shopping Cart page</li>
-          					<li>• Implemented search bar</li>
-          					<li>• Managed project</li>
-          				</ul>
-          				<h5>Angela Gavin</h5>
-          				<ul>
-          					<li>• Created the Read More page</li>
-          					<li>• Managed the database</li>
-          					<li>• Wrote report</li>
-          				</ul>
-          				</p>
-          				<h5>Alison Der</h5>
-          				<ul>
-          					<li>• Created the Home page</li>
-          					<li>• Styled whole website with css</li>
-          				</ul>
-        			</div>
-        			<div class="card-action">
-          				<a href="#">This is a link</a>
-        			</div> -->
+      		<div class="card-stacked">
+        		<div class="card-content">
+              <p class="smol">
+                <!-- The objective of this project was to create a travel website that is both responsive and user-friendly. <em>Plan Your Travel</em> features 20 famous attractions around the world from 5 different continents, and two different countries per continent. We used HTML, CSS, PHP, sql, Javascript, JQuery, Ajax, and MaterializeCSS to design and implement this website. -->
+                <b>Hello!</b><br>
+                We are a small team of web developers that implemented <em>Plan Your Travel</em>. This travel website features 20 famous attractions around the world from 5 different continents, and two different countries per continent. Users are able to view information about the attractions and check out reviews. In addition to this, our website includes two different travel plans to make trip-planning easy and simple.<br><br>
+
+                With the use of MaterializeCSS, PHP, sql, HTML, CSS, Javascript, JQuery, and Ajax, we were able to make this website both responsive and user-friendly. However, we would like to develop this project further so if you have any questions or suggestions on how to improve our website, feel free to contact anyone on our team via the 'Contact' page. Your feedback is greatly appreciated!<br><br>
+
+                Thanks,<br>
+                Team 25
+              </p>
+        			<!-- <ul class="collection" style="border: none;">
+                <li class="collection-item avatar">
+                <img src="assets/img/dog.png" class="circle">
+                <span class="title">Nick De Santo</span>
+                  <p>
+                    Hi! Welcome to our travel website! In order for this website to be responsive and pretty, we used MaterializeCSS, Ajax, Javascript, and JQuery. 
+                  </p><ul>
+                    <li>• Created the Shopping Cart Page</li>
+                    <li>• Implemented search bar</li>
+                    <li>• Managed project</li>
+                  </ul></p>
+                </li>
+                <li class="collection-item avatar">
+                <img src="assets/img/dog1.png" class="circle">
+                <span class="title">Angela Gavin</span>
+                  <p><ul>
+                    <li>• Created the Read More page</li>
+                    <li>• Managed the database</li>
+                    <li>• Wrote report</li>
+                  </ul></p>
+                </li>
+                <li class="collection-item avatar">
+                <img src="assets/img/dog2.png" class="circle">
+                <span class="title">Alison Der</span>
+                  <p><ul>
+                    <li>• Created the Home page</li>
+                    <li>• Styled whole website with css</li>
+                  </ul></p>
+                </li> -->
+              </ul>
       			</div>
-    		</div>
-  		</div>
-	</div>
+    		  </div>
+  		  </div>
+      </div>
+    </div>
 <body>
