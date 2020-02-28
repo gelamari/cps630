0f9fd27041acdeec9e6842bcfba669e580a42bcd
gelamari <angela.gavin@ryerson.ca>
2020-02-28 03:11:38 -0500

    just font things in the about us

--- Project/assets/includes/header.php
+++ Project/assets/includes/header.php
@@ -1,5 +1,6 @@
 <head>
     <title><?php echo $title; ?></title>
+<link href="https://fonts.googleapis.com/css?family=Dancing+Script:600&display=swap" rel="stylesheet">
 
     <link rel="stylesheet" type="text/css" href="assets/css/style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
