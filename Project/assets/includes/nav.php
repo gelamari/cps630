e3037b33f23430639e57fb989198130aa38cfd1a
Nick De Santo <ndesanto@ryerson.ca>
2020-02-27 23:54:39 -0500

    added search modal

--- Project/assets/includes/nav.php
+++ Project/assets/includes/nav.php
@@ -8,10 +8,9 @@
       <ul class="hide-on-med-and-down">
             <li id="home"><a href="homepage.php">Home</a></li>
             <li id="about"><a href="about.php">About</a></li>
-            <li id="contact"><a href="contact.php">Contact</a></li>
+            <li id="contact" class=><a href="contact.php">Contact</a></li>
+            <li id="search"><a class="modal-trigger" href="#modal1">Search</a></li>
             <li class="right"><a href="cart.php"><i class="material-icons right">shopping_cart</i>Cart</a></li>
-      
-
       </ul>
     </div>
 </nav>
@@ -23,10 +22,41 @@
       <li id="cart" class="rightnav-item"><a href="cart.php"></span>Cart</a></li>
   </ul>
 </div>
+
+<!-- Modal Structure -->
+<div id="modal1" class="modal bottom-sheet">
+  <div class="modal-content">
+    <h4>Search Database:</h4>
+    <input type="text" id="search"/>
+    <div id="searchOutput"></div>
+  </div>
+  <div class="modal-footer">
+    <a href="#!" id="searchButton" class="waves-effect waves-green btn-flat">Search</a>
+  </div>
+</div>
+
 <script>
       $("#<?php echo $page; ?>").addClass('active');
 
       $(document).ready(function(){
             $('.sidenav').sidenav();
-      });
+            $('.modal').modal();
+
+            $('#searchButton').on('click', function(){
+                  var request = $.ajax({
+				url: "test.php",
+				type: "POST",
+				data: { search : $(this).val() }
+                        });
+                        request.done(function(msg) {
+                              $("#searchOutput").append(msg);
+
+
+                        });
+                        request.fail(function(jqXHR, textStatus) {
+                              alert( "Request failed: " + textStatus );
+                        });
+                  });
+            });
+            
 </script>
--- Project/test.php
+++ Project/test.php
@@ -141,4 +141,46 @@ if (isset($_POST["attraction"])) {
 	echo implode("|", $outputArray);
 }
 
+
+if (isset($_POST["search"])) {
+    $search = 'Italy';
+
+	$conn = OpenCon(); 
+	$outputResults = array();
+	
+	$queryAttractions = "SELECT * FROM `attractions` WHERE (CONVERT(`a_id` USING utf8) LIKE '%$search%' OR CONVERT(`title` USING utf8) LIKE '%$search%' OR CONVERT(`date_of_creation` USING utf8) LIKE '%$search%' OR CONVERT(`founder_name` USING utf8) LIKE '%$search%' OR CONVERT(`dimensions` USING utf8) LIKE '%$search%' OR CONVERT(`location` USING utf8) LIKE '%$search%' OR CONVERT(`country` USING utf8) LIKE '%$search%' OR CONVERT(`continent` USING utf8) LIKE '%$search%')";
+	$queryOrders = "SELECT *  FROM `orders` WHERE (CONVERT(`id` USING utf8) LIKE'%$search%' OR CONVERT(`plan_id` USING utf8) LIKE'%$search%' OR CONVERT(`num_travelers` USING utf8) LIKE'%$search%' OR CONVERT(`ages` USING utf8) LIKE'%$search%' OR CONVERT(`total` USING utf8) LIKE'%$search%')";
+	$queryPhotos = "SELECT *  FROM `photos` WHERE (CONVERT(`p_id` USING utf8) LIKE'%$search%' OR CONVERT(`img_path` USING utf8) LIKE'%$search%' OR CONVERT(`a_id` USING utf8) LIKE'%$search%')";
+	$queryPlans = "SELECT *  FROM `plans` WHERE (CONVERT(`id` USING utf8) LIKE'%$search%' OR CONVERT(`start_date` USING utf8) LIKE'%$search%' OR CONVERT(`duration` USING utf8) LIKE'%$search%' OR CONVERT(`fare` USING utf8) LIKE'%$search%' OR CONVERT(`total_price` USING utf8) LIKE'%$search%' OR CONVERT(`purchased` USING utf8) LIKE'%$search%' OR CONVERT(`map_link` USING utf8) LIKE'%$search%' OR CONVERT(`description` USING utf8) LIKE'%$search%')";
+	$queryReviews ="SELECT *  FROM `reviews` WHERE (CONVERT(`r_id` USING utf8) LIKE'%$search%' OR CONVERT(`name` USING utf8) LIKE'%$search%' OR CONVERT(`rating` USING utf8) LIKE'%$search%' OR CONVERT(`review` USING utf8) LIKE'%$search%' OR CONVERT(`a_id` USING utf8) LIKE'%$search%' OR CONVERT(`date_posted` USING utf8) LIKE'%$search%')";
+
+	$queries = array($queryAttractions, $queryOrders, $queryOrders, $queryPhotos, $queryPlans, $queryReviews);
+
+	foreach($queries as $query) {
+		$searchResult = $conn->query($query);
+		echo("<table class=\"responsive-table striped\">");
+		$first_row = true;
+		try {
+			while ($row = $searchResult->fetch_assoc()) {
+				if ($first_row) {
+					$first_row = false;
+					// Output header row from keys.
+					echo '<tr>';
+					foreach($row as $key => $field) {
+						echo '<th>' . htmlspecialchars($key) . '</th>';
+					}
+					echo '</tr>';
+				}
+				echo '<tr>';
+				foreach($row as $key => $field) {
+					echo '<td style="overflow: hidden; max-width: 300px;">' . htmlspecialchars($field) . '</td>';
+				}
+				echo '</tr>';
+			}
+		} catch (Exception $e) {echo '';}
+		echo("</table>");
+	}
+    CloseCon($conn);
+}
+
 ?>
